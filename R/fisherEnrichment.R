20edbb547be4b4c7d68f9bc976c1fa7ce5f5313b
Thanneer Malai Perumal <th.vairam@gmail.com>
2016-03-11 17:45:23 +0000

    Modified filterGeneSets.R and fisherEnrichment.R

--- R/fisherEnrichment.R
+++ R/fisherEnrichment.R
@@ -1,20 +1,26 @@
+# Function to perform Fishers enrichment analysis
 fisherEnrichment <- function(genesInSignificantSet, # A character vector of differentially expressed or some significant genes to test
                              genesInGeneSet, # A character vector of genes in gene set like GO annotations, pathways etc...
-                             genesInBackground # Background genes that are 
-                             ){
+                             genesInBackground # Background genes that are
+){
   genesInSignificantSet = intersect(genesInSignificantSet, genesInBackground) # back ground filtering
   genesInNonSignificantSet = base::setdiff(genesInBackground, genesInSignificantSet)
   genesInGeneSet = intersect(genesInGeneSet, genesInBackground) # back ground filtering
   genesOutGeneSet = base::setdiff(genesInBackground,genesInGeneSet)
-  
+
   pval = fisher.test(
-    matrix(c(length(intersect(genesInGeneSet, genesInSignificantSet)),             
+    matrix(c(length(intersect(genesInGeneSet, genesInSignificantSet)),
              length(intersect(genesInGeneSet, genesInNonSignificantSet)),
              length(intersect(genesOutGeneSet, genesInSignificantSet)),
-             length(intersect(genesOutGeneSet, genesInNonSignificantSet))), 
+             length(intersect(genesOutGeneSet, genesInNonSignificantSet))),
            nrow=2, ncol=2),
     alternative="greater")
+  OR = (length(intersect(genesInGeneSet, genesInSignificantSet)) * length(intersect(genesOutGeneSet, genesInNonSignificantSet))) / (length(intersect(genesInGeneSet, genesInNonSignificantSet)) * length(intersect(genesOutGeneSet, genesInSignificantSet)))
   return(data.frame(pval = pval$p.value,
                     ngenes = length(genesInGeneSet),
-                    noverlap = length(intersect(genesInGeneSet, genesInSignificantSet))))
+                    noverlap = length(intersect(genesInGeneSet, genesInSignificantSet)),
+                    Odds.Ratio = OR,
+                    OverlapedGenes = paste(intersect(genesInGeneSet, genesInSignificantSet), collapse = '|')
+  )
+  )
 }

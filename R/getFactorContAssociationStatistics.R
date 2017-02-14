29c1d4daa7cf07d927c730d07a056b5790912adc
Thanneer Malai Perumal <thanneer.perumal@sagebase.org>
2017-02-14 19:14:03 +0000

    Added own version of R function to compute ICC using one-way anova. Accordingly modified getFactorContAssociationStatistics.R codes.

--- R/getFactorContAssociationStatistics.R
+++ R/getFactorContAssociationStatistics.R
@@ -2,15 +2,14 @@
 # Inspired from http://stats.stackexchange.com/questions/108007/correlations-with-categorical-variables
 getFactorContAssociationStatistics <- function(factorContNames,COVARIATES, na.action='remove',
                                                alpha = 0.05){
-  require(psych)
+
   factorContNames = factorContNames %>% unlist %>% as.character()
 
   if (na.action == "remove")
     COVARIATES = na.omit(COVARIATES[,factorContNames])
 
-  COVARIATES = sapply(COVARIATES, as.numeric)
-  stats = psych::ICC(COVARIATES, missing = T, alpha = alpha)
+  class.names = sapply(COVARIATES, is.factor)
+  stats = ICC(colnames(COVARIATES)[class.names], colnames(COVARIATES)[!class.names], COVARIATES, alpha = alpha)
 
-  return(data.frame(Estimate = stats$results['Single_fixed_raters','ICC'],
-                    Pval = stats$results['Single_fixed_raters','p']))
+  return(data.frame(Estimate = stats$ICC, Pval = stats$p.val))
 }
